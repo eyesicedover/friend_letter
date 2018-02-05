11641dd58fe0f68f1d2b27e737089a72dbf56c1b
Ian Goodrich and Stephanie Faber <ian@>
2018-02-05 10:25:24 -0800

    create project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using FriendLetter.Models;
+
+namespace FriendLetter.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Produces("text/html")]
+        [Route("/")]
+        public ActionResult Letter()
+        {
+            LetterVariable myLetterVariable = new LetterVariable();
+            myLetterVariable.SetRecipient("Eric");
+            myLetterVariable.SetSender("John");
+            return View(myLetterVariable);
+        }
+
+        [Route("/hello")]
+        public string Hello() { return "Hello friend!"; }
+
+        [Route("/goodbye")]
+        public string Goodbye() { return "Goodbye friend."; }
+
+        [Route("/journal")]
+        public ActionResult Journal()
+        {
+            return View();
+        }
+
+        [Route("/form")]
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        [Route("/greeting_card")]
+        public ActionResult GreetingCard()
+        {
+            LetterVariable myLetterVariable = new LetterVariable();
+            myLetterVariable.SetRecipient(Request.Query["recipient"]);
+            myLetterVariable.SetSender(Request.Query["sender"]);
+            return View("greeting_card", myLetterVariable);
+        }
+    }
+}
